5079becc6f2d96a69a804e91027aa9770de6c919
Adama55 <mafahfof55@gmail.com>
2023-11-04 20:55:27 +0100

    mise à jour du schema

--- classes/schema_dot.py
+++ classes/schema_dot.py
@@ -2,18 +2,24 @@ from pydantic import BaseModel
 from datetime import datetime
 
 class MeetingRoom(BaseModel):
-    room_name: str
+    id: int  # Identifiant unique de l'annonce
+    owner_id: int  # Identifiant de l'utilisateur qui a publié l'annonce
+    title: str
+    description: str
     location: str
-    address: str
     capacity: int
     priceOnHours: float
+    click_count: int  # Nombre de clics sur l'annonce
+    is_available: bool
 
-class Reservation(BaseModel):
-    room: MeetingRoom
-    start_time: datetime
-    end_time: datetime
-    user_id: int
+class Click(BaseModel):
+    id: int  # Identifiant unique du clic
+    user_id: int  # Identifiant de l'utilisateur qui a effectué le clic
+    room_listing_id: int  # Identifiant de l'annonce cliquée
+    click_time: datetime  # Date et heure du clic
 
 class User(BaseModel):
-    email: str
-    password: str 
+    id: int  # Identifiant unique de l'utilisateur
+    username: str 
+    email: str  
+    password: str  
