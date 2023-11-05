d23557b717c385332522e255cb2015ce6096229d
Adama55 <mafahfof55@gmail.com>
2023-11-05 22:45:10 +0100

    updat main

--- main.py
+++ main.py
@@ -4,12 +4,19 @@ from fastapi import FastAPI
 from documentation.description import api_description
 from documentation.tags import tags_metadata
 
+#import router
+import routers.router_auth
+import routers.router_clicRoom
+import routers.router_meetingRoom
+import routers.router_strip
+
 app = FastAPI(
     title="MeetingRoom Reservation",
     description=api_description,
     openapi_tags=tags_metadata
 )
 
-@app.get("/")
-async def root():
-    return { "message" : "Hello World"}
+app.include_router(routers.router_auth.router)
+app.include_router(routers.router_clicRoom.router)
+app.include_router(routers.router_meetingRoom.router)
+app.include_router(routers.router_strip.router)
